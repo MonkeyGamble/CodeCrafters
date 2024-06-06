82de3b9106d7ba27c11f1b4a70a91e8b7a9dfac3
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-06-06 14:00:10 +0200

    discounted items page

--- code_crafters_app/src/components/pages/DiscountedItemsPage/index.js
+++ code_crafters_app/src/components/pages/DiscountedItemsPage/index.js
@@ -1,5 +1,27 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import DiscountProducts from '../../DiscountProducts';
+import { getAllProducts } from '../../../asyncActions/products';
 import s from './DiscountedItemsPage.module.css';
 
-export default function DiscountedItemsPage() {
-	return <div>DiscountedItemsPage</div>;
+export default function DiscountItemsPage() {
+	const dispatch = useDispatch();
+
+	const discountProducts = useSelector(
+		state => state.products.discountProducts
+	);
+
+	useEffect(() => {
+		dispatch(getAllProducts());
+	}, [dispatch]);
+
+	return (
+		<div>
+			<DiscountProducts
+				products={discountProducts}
+				header='Discounted Items'
+				styles={s}
+			/>
+		</div>
+	);
 }

c7e4f55ccb3634410b44684f2107a42391306a01
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-06-10 21:44:34 +0200

    categories fix

--- code_crafters_app/src/components/pages/HomePage/index.js
+++ code_crafters_app/src/components/pages/HomePage/index.js
@@ -1,16 +1,16 @@
 import DiscountBanner from './DiscountBanner';
 import DiscountProducts from './DiscountProducts';
 import s from './HomePage.module.css';
-
+import cs from './Categories/Categories.module.css';
+import Categories from './Categories';
 import DiscountUserForm from './DiscountUserForm';
 import '../../../Global.css';
-import CategoriesHomePage from './Categories/index';
 
 export default function HomePage() {
 	return (
 		<main>
 			<DiscountBanner />
-			<CategoriesHomePage limit={4} />
+			<Categories limit={4} style={cs} />
 			<DiscountUserForm />
 			<DiscountProducts className={s.discount_products} />
 		</main>

f20a9d29accf181e750f3122dbf266a7bdc7693f
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-07-05 16:53:54 +0200

    fix

---
+++ code_crafters_app/src/utils/handleBasketClick.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux';
+import { useBasketActions } from '../redux/actions/basket';
+
+const useHandleBasketClick = product => {
+	const inBasket = useSelector(state =>
+		state.basket.basket.items.some(baskProduct => baskProduct.id === product.id)
+	);
+	const { addProductToBasket, removeProductFromBasket } = useBasketActions();
+
+	const handleBasketClick = e => {
+		e.preventDefault();
+		e.stopPropagation();
+		if (inBasket) {
+			removeProductFromBasket(product.id);
+		} else {
+			addProductToBasket({ ...product, count: 1 });
+		}
+	};
+	return handleBasketClick;
+};
+
+export default useHandleBasketClick;

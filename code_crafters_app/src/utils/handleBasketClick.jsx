3f40ee95b6f04dcaa4eaee60ecc6ef4319714ec5
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2024-07-07 22:12:36 +0200

    fix product.id

--- code_crafters_app/src/utils/handleBasketClick.jsx
+++ code_crafters_app/src/utils/handleBasketClick.jsx
@@ -3,15 +3,21 @@ import { useBasketActions } from '../redux/actions/basket';
 
 const useHandleBasketClick = product => {
 	const inBasket = useSelector(state =>
-		state.basket.basket.items.some(
-			baskProduct => baskProduct.id === product?.id
-		)
+		product?.id
+			? state.basket.basket.items.some(
+					baskProduct => baskProduct.id === product.id
+			  )
+			: false
 	);
 	const { addProductToBasket, removeProductFromBasket } = useBasketActions();
 
 	const handleBasketClick = e => {
 		e.preventDefault();
 		e.stopPropagation();
+		if (!product) {
+			console.error('Product is null or undefined');
+			return;
+		}
 		if (inBasket) {
 			removeProductFromBasket(product.id);
 		} else {
